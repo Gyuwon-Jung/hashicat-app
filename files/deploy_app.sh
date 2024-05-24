a3f9fbb27f2ff06547834b7e58faa944c0cd8548
Gyuwon-Jung <licmm0520@gmail.com>
2024-05-24 04:18:55 +0000

    Updated HTML payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,6 +11,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
+  <h1>Welcome to ${PREFIX}'s app. Replace this text with your own.</h1>
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own.
